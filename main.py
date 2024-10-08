006fcc33baaa94e2bdbe6fc3cbe40f515308d20e
graceattalla <grace.attalla@gmail.com>
2024-10-08 15:18:08 -0700

    is_processing_command flag addition
    Attempt to stop UI from not responding, potentially due to backlogging of mental commands during keyboard press. Unsuccessful.

--- main.py
+++ main.py
@@ -20,8 +20,14 @@ class WelcomeScreen(QtWidgets.QMainWindow):
         #passes in the current instance main_ui self into the LiveAdvance
         self.live_advance = LiveAdvance(self, your_app_client_id, your_app_client_secret) #create instance and pass self (UI) to it (added)
 
-    #
     def on_new_cmd(self, cmd): #cmd is the data coming from live_advance.py
+        
+        if self.is_processing_command == True:
+            print(self.is_processing_command)
+            return # Ignore new commands until the current one is processed
+        print(self.is_processing_command)
+        self.is_processing_command = True
+
         self.OutPutLabel.setText(f"Current Emotiv BCI output: {cmd['action']}, {cmd['power']}")
 
         self.simulate_key_press(cmd['action'], cmd['power'])
@@ -42,10 +48,13 @@ class WelcomeScreen(QtWidgets.QMainWindow):
             #simulate the keyboard based on the selected value
             kb.press(selected_value.lower())
             print("Key pressed: ", selected_value)
-            # time.sleep(0.1) #only get the next mental command after the first push value is finished
+            time.sleep(0.1) #only get the next mental command after the first push value is finished
+            # Create a QTimer to release the key after 0.1 seconds
+            # QtCore.QTimer.singleShot(100, lambda: self.release_key(selected_value.lower()))  # Release after 100 ms
             kb.release(selected_value.lower())
 
-
+        #allow for a new mental command to be processed (put outside if statement so that neutral commands can be considered)
+        self.is_processing_command = False
 
 
     #name is the box selected by the user and "value" is the new value they select from the drop down
@@ -61,6 +70,7 @@ class WelcomeScreen(QtWidgets.QMainWindow):
 
     def start_mapping(self):
         print('start_mapping')
+        self.is_processing_command = False #initialize so that the first command can be processed
         self.live_advance.start(trained_profile_name)
 
     def pause_mapping(self):
